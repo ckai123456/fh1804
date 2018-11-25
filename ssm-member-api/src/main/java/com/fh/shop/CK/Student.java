5fa360b28a3debbc2ff77c8899897dbc53d5e08f
陈凯18639622189 <645739476@qq.com>
2018-11-25 23:42:17 +0800

    first commit

---
+++ ssm-member-api/src/main/java/com/fh/shop/CK/Student.java
@@ -0,0 +1,42 @@
+package com.fh.shop.CK;
+
+/**
+ * <pre>项目名称：
+ * 类名称：Student
+ * 类描述：
+ * 创建人：陈凯 Mr_Chen__K@163.com
+ * 创建时间：2018/11/25 22:11
+ * 修改人：陈凯 Mr_Chen__K@163.com
+ * 修改时间：2018/11/25 22:11
+ * 修改备注：
+ * @version </pre>
+ */
+public class Student {
+    private Integer id;
+    private String stuName;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
